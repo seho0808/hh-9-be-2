219c757db920813a7f4d65b9911200c5fef4889b
Seho Lee <seho0808@vt.edu>
2025-07-17 23:01:48 +0900

    feat: app.ts module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from "@nestjs/common";
 import { DatabaseModule } from "./database/database.module";
+import { AuthModule } from "./auth/auth.module";
+import { UserModule } from "./user/user.module";
+import { ProductModule } from "./product/product.module";
+import { WalletModule } from "./wallet/wallet.module";
+import { CouponModule } from "./coupon/coupon.module";
+import { OrderModule } from "./order/order.module";
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    // DatabaseModule,
+    AuthModule,
+    UserModule,
+    ProductModule,
+    WalletModule,
+    CouponModule,
+    OrderModule,
+  ],
   controllers: [],
   providers: [],
 })
