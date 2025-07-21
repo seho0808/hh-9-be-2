7703ee09be82fc35fbeb3e9e9f354fde5f403ab9
Seho Lee <seho0808@vt.edu>
2025-07-21 20:04:08 +0900

    feat(product): usecase 구현
    - 기본 product READ usecase 구현
    - 동시성 처리를 위한 reserve, release, confirm usecsae 구현
    - user은 usecase를 application 계층에 구현했었음. 그런데 product에서 구현하다보니 exception, dto들이 application 고유한 것들이 생기다 보니 usecase가 다시 domain 계층으로 돌아오는 것이 맞아보였음.

---
+++ src/product/domain/exceptions/product.exceptions.ts
@@ -0,0 +1,70 @@
+export abstract class ProductDomainError extends Error {
+  abstract readonly code: string;
+
+  constructor(message: string) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
+export class ProductNotFoundError extends ProductDomainError {
+  readonly code = "PRODUCT_NOT_FOUND";
+
+  constructor(productId: string) {
+    super(`상품을 찾을 수 없습니다. ID: ${productId}`);
+  }
+}
+
+export class InsufficientStockError extends ProductDomainError {
+  readonly code = "INSUFFICIENT_STOCK";
+
+  constructor(
+    productId: string,
+    availableStock: number,
+    requestedAmount: number
+  ) {
+    super(
+      `재고가 부족합니다. 상품ID: ${productId}, 사용가능: ${availableStock}, 요청: ${requestedAmount}`
+    );
+  }
+}
+
+export class InvalidQuantityError extends ProductDomainError {
+  readonly code = "INVALID_QUANTITY";
+
+  constructor(quantity: number) {
+    super(`유효하지 않은 수량입니다: ${quantity}`);
+  }
+}
+
+export class InactiveProductError extends ProductDomainError {
+  readonly code = "INACTIVE_PRODUCT";
+
+  constructor(productId: string) {
+    super(`비활성화된 상품입니다. ID: ${productId}`);
+  }
+}
+
+export class StockReservationNotFoundError extends ProductDomainError {
+  readonly code = "STOCK_RESERVATION_NOT_FOUND";
+
+  constructor(stockReservationId: string) {
+    super(`예약된 재고가 없습니다. ID: ${stockReservationId}`);
+  }
+}
+
+export class StockReservationNotActiveError extends ProductDomainError {
+  readonly code = "STOCK_RESERVATION_NOT_ACTIVE";
+
+  constructor(stockReservationId: string) {
+    super(`재고 예약이 비활성화되었습니다. ID: ${stockReservationId}`);
+  }
+}
+
+export class StockReservationExpiredError extends ProductDomainError {
+  readonly code = "STOCK_RESERVATION_EXPIRED";
+
+  constructor(stockReservationId: string) {
+    super(`재고 예약이 만료되었습니다. ID: ${stockReservationId}`);
+  }
+}
