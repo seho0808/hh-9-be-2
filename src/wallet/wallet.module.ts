ae8e838d1cbe54afe9a0632c1e11733d10e57a01
Seho Lee <seho0808@vt.edu>
2025-07-24 15:44:25 +0900

    feat(wallet): wallet.module.ts 구현

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -1,12 +1,44 @@
-import { Module } from "@nestjs/common";
-import { WalletController } from "./wallet.controller";
-import { WalletMockService } from "./services/wallet.mock.service";
+import { Module, forwardRef } from "@nestjs/common";
+import { WalletController } from "./infrastructure/http/wallet.controller";
 import { AuthModule } from "../auth/auth.module";
+import { UserBalanceRepository } from "./infrastructure/persistence/use-balance.repository";
+import { PointTransactionRepository } from "./infrastructure/persistence/point-transaction.repository";
+import { WalletApplicationService } from "./application/wallet.service";
+import { ChargePointsUseCase } from "./domain/use-cases/charge-points.use-case";
+import { RecoverPointsUseCase } from "./domain/use-cases/recover-points.use-case";
+import { UsePointsUseCase } from "./domain/use-cases/use-points.use-case";
+import { GetUserPointsUseCase } from "./domain/use-cases/get-user-points.use-case";
+import { CreateUserBalanceUseCase } from "./domain/use-cases/create-user-balance.use-case";
+import { UserBalanceTypeOrmEntity } from "./infrastructure/persistence/orm/user-balance.typeorm.entity";
+import { PointTransactionTypeOrmEntity } from "./infrastructure/persistence/orm/point-transaction.typeorm.entity";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    forwardRef(() => AuthModule),
+    TypeOrmModule.forFeature([
+      UserBalanceTypeOrmEntity,
+      PointTransactionTypeOrmEntity,
+    ]),
+  ],
   controllers: [WalletController],
-  providers: [WalletMockService],
-  exports: [WalletMockService],
+  providers: [
+    WalletApplicationService,
+    ChargePointsUseCase,
+    RecoverPointsUseCase,
+    UsePointsUseCase,
+    GetUserPointsUseCase,
+
+    CreateUserBalanceUseCase,
+    {
+      provide: "UserBalanceRepositoryInterface",
+      useClass: UserBalanceRepository,
+    },
+    {
+      provide: "PointTransactionRepositoryInterface",
+      useClass: PointTransactionRepository,
+    },
+  ],
+  exports: [WalletApplicationService],
 })
 export class WalletModule {}
