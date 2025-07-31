f3ea787dc442517975f98e40a9a0845498b291c7
Seho Lee <seho0808@vt.edu>
2025-07-31 21:11:52 +0900

    refactor: 모든 레포지토리 인터페이스 제거
    - 영속성 계층은 db 하나만 쓰기 때문에 레포지토리 인터페이스 제거

--- src/product/domain/interfaces/stock-reservation.repository.interface.ts
+++
@@ -1,7 +0,0 @@
-import { StockReservation } from "../entities/stock-reservation.entity";
-
-export interface StockReservationRepositoryInterface {
-  save(stockReservation: StockReservation): Promise<StockReservation>;
-  findById(id: string): Promise<StockReservation | null>;
-  findByIdempotencyKey(idempotencyKey: string): Promise<StockReservation[]>;
-}
