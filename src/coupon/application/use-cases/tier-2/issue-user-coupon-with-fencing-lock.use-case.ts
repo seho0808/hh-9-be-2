e13d07786bc78dd879407a0e31044e73a4acdd94
Seho Lee <seho0808@vt.edu>
2025-08-14 02:02:31 +0900

    feat(coupon): 쿠폰 발급 전략별로 유스케이스, 엔드포인트 구현

---
+++ src/coupon/application/use-cases/tier-2/issue-user-coupon-with-fencing-lock.use-case.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@nestjs/common";
+import { Coupon } from "@/coupon/domain/entities/coupon.entity";
+import { UserCoupon } from "@/coupon/domain/entities/user-coupon.entity";
+import { FencingLockService } from "@/common/infrastructure/locks/fencing-lock.service";
+import { IssueUserCouponUseCase } from "../tier-1-in-domain/issue-user-coupon.use-case";
+
+export interface IssueUserCouponWithFencingLockCommand {
+  couponId: string;
+  userId: string;
+  couponCode: string;
+  idempotencyKey: string;
+}
+
+export interface IssueUserCouponWithFencingLockResult {
+  coupon: Coupon;
+  userCoupon: UserCoupon;
+}
+
+@Injectable()
+export class IssueUserCouponWithFencingLockUseCase {
+  constructor(
+    private readonly fencingLockService: FencingLockService,
+    private readonly issueUserCouponUseCase: IssueUserCouponUseCase
+  ) {}
+
+  async execute(
+    command: IssueUserCouponWithFencingLockCommand
+  ): Promise<IssueUserCouponWithFencingLockResult> {
+    const lockKey = `fencing:coupon:issue:${command.couponId}`;
+
+    return await this.fencingLockService.withLock(lockKey, async () => {
+      return await this.issueUserCouponUseCase.execute(command);
+    });
+  }
+}
