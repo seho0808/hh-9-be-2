0029fd7e017c038dca8071a596cd4680e9952349
Seho Lee <seho0808@vt.edu>
2025-08-28 22:43:30 +0900

    feat: AfterCommitEmit 데코레이터 구현
    - 함수 호출 후에 이벤트 생성 어노테이션 구현

---
+++ src/common/decorators/after-commit-emit.decorator.ts
@@ -0,0 +1,31 @@
+import { EventEmitter2 } from "@nestjs/event-emitter";
+
+export function AfterCommitEmit<T extends (...args: any[]) => any>(
+  eventName: string,
+  buildPayload: (result: Awaited<ReturnType<T>>, args: Parameters<T>) => any
+): MethodDecorator {
+  return (_target, _propertyKey, descriptor) => {
+    const original = descriptor.value as (...args: any[]) => any;
+    const wrapped = async function (
+      this: { eventEmitter?: EventEmitter2 },
+      ...args: Parameters<T>
+    ): Promise<Awaited<ReturnType<T>>> {
+      const result = (await original.apply(this, args)) as Awaited<
+        ReturnType<T>
+      >;
+
+      const emitter = this.eventEmitter;
+      if (emitter) {
+        try {
+          await emitter.emitAsync(eventName, buildPayload(result, args));
+        } catch {
+          // swallow to avoid affecting caller flow
+        }
+      }
+
+      return result;
+    } as any;
+    (descriptor as any).value = wrapped as any;
+    return descriptor;
+  };
+}
